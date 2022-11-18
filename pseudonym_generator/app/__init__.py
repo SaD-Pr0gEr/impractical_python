9cfd414b00bb56b20341124e3469fb495c3ecf94
SaD-Pr0gEr <sad.pr0ger@gmail.com>
2022-11-18 23:23:52 +0300

    pig latin

--- pseudonym_generator/app/__init__.py
+++ pseudonym_generator/app/__init__.py
@@ -12,7 +12,8 @@ def app() -> None:
 
     :return: None
     """
-    print("Добро пожаловать в бессмысленную программу по генерации бессмысленных имён!")
+    print("Добро пожаловать в бессмысленную программу "
+          "по генерации бессмысленных имён!")
     while True:
         f_name = random.choice(group_1)
         l_name = random.choice(group_2)
@@ -20,4 +21,29 @@ def app() -> None:
         try_again = input(f"{Colors.PINK}Попробовать ещё?(y/n): ").lower()
         if try_again == "n":
             print(f"{Colors.RED}Пока!")
+            break
+
+
+def project_pig_latin() -> None:
+    """Practice: Program pig latin.
+
+    User inputs word.
+    If its starts with 'vowel' letter program endures to
+        the end and adds to the end of this word 'ay'.
+    If its starts with 'consonant' letter program just adds to
+        the end of this word 'way'.
+
+    :return: None
+    """
+    vowels = "aiueo"
+
+    while True:
+        user_word = input("Write some word: ").lower()
+        if user_word[0] in vowels:
+            print(f"{Colors.GREEN}{user_word}way")
+        else:
+            print(f"{Colors.YELLOW}{user_word[1:]}{user_word[0]}ay")
+        try_again = input(f"{Colors.PINK}Try again?(y/n): ").lower()
+        if try_again == "n":
+            print(f"{Colors.RED}Bye!")
             break
--- pseudonym_generator/run.py
+++ pseudonym_generator/run.py
@@ -1,6 +1,7 @@
 """Enter to our application."""
 
-from app import app
+from app import app, project_pig_latin
 
 if __name__ == "__main__":
     app()
+    project_pig_latin()
